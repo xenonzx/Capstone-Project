a8ead1303019638c5681439a4a104da17a0cb64a
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2017-01-25 21:33:19 +0200

    adding jop dispatcher as a dependency in gradle and to extend BankRatesJobService.java

--- app/src/main/java/com/luxtech_eg/nanodegree/dakhakhny/omla/sync/BankRatesJobService.java
+++ app/src/main/java/com/luxtech_eg/nanodegree/dakhakhny/omla/sync/BankRatesJobService.java
@@ -1,9 +1,9 @@
 package com.luxtech_eg.nanodegree.dakhakhny.omla.sync;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.content.Intent;
 import android.util.Log;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
 //TODO extend from jop dispatcher
 public class BankRatesJobService extends JobService {
